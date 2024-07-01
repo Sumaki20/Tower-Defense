2b645c9002dba5165519fa09605be10b6b95019c
Pongsakorn Sinawat <59215179+Sumaki20@users.noreply.github.com>
2024-07-01 08:24:08 +0700

    Test1

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -6,7 +6,8 @@ public class BaseEnemy : MonoBehaviour
 {
     public float moveSpeed = 3f;
 
-    [SerializeField]private Transform[] enemy;
+    public Transform[] wayPoint;
+    private int currentWaypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,14 @@ public class BaseEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDirection = wayPoint[currentWaypointIndex + 1].position - transform.position;
+
+        if(moveDirection.magnitude <= moveSpeed * Time.deltaTime)
+        {
+            currentWaypointIndex++;
+            transform.position = wayPoint[currentWaypointIndex].position;
+        }
+        transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime,Space.World);
+        Debug.Log(moveDirection.magnitude);
     }
 }
