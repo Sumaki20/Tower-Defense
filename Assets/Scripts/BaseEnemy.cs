46e7a857a863040fb7adc0f5478e20bd81701788
Pongsakorn Sinawat <59215179+Sumaki20@users.noreply.github.com>
2024-07-01 07:23:25 +0700

    Initial commit

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+
+    [SerializeField]private Transform[] enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
